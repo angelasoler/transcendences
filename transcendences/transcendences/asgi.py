35026d5cdfa1dcba9aa99b6a5bf69e5629d1747e
angelasoler <manzanaembicicleta@gmail.com>
2024-09-24 11:56:43 -0300

    fix typo

--- transcendences/transcendences/asgi.py
+++ transcendences/transcendences/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for trancendences project.
+ASGI config for transcendences project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trancendences.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendences.settings')
 
 application = get_asgi_application()
--- transcendences/transcendences/wsgi.py
+++ transcendences/transcendences/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for trancendences project.
+WSGI config for transcendences project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trancendences.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendences.settings')
 
 application = get_wsgi_application()
