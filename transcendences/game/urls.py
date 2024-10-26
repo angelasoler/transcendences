603af9d361d91d13a1f9412a0b8392d47c1e76fc
Renato Crescenti <96125541+rscres@users.noreply.github.com>
2024-10-26 14:39:16 +0000

    Fix: requests caindo no catch_all em user/urls.py. Move catch_all e not_found para urls.py do projeto e remove dos outros urls.py

--- transcendences/game/urls.py
+++ transcendences/game/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/join_or_create_room/', views.join_or_create_room, name='join_or_create_room'),
-    re_path(r'^.*$', views.not_found, name='catch_all')
+    path('api/create_tournament/', views.create_tournament, name='create_tournament'),
+    path('api/tournament_matchups/<str:tournament_id>/', views.get_tournament_matchups, name='get_tournament_matchups'),
+    path('api/update_bracket/<str:tournament_id>/', views.update_bracket, name='update_bracket'),
+    path('start_next_game/<str:tournament_id>/', views.start_next_game, name='start_next_game'),
 ]
